1e17f9a60f655bd1723027206018405d2e100d85
unlimitedglassworks <66311794+TubofGlasses@users.noreply.github.com>
2024-02-11 18:13:21 +0800

    url fix

--- baddestDemo/IDSapp/urls.py
+++ baddestDemo/IDSapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.view_dashboard, name = 'view_dashboard'),
     path('dashboard', views.view_dashboard, name = 'view_dashboard'),
     path('home', views.view_home, name = 'view_home'),
     path('progress_tracker', views.view_progress, name = 'view_progress'),
